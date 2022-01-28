20193c2cea9c7fa3671934ffcffc0c2b5f691ba9
EdWright <snl290995@gmail.com>
2022-01-28 20:34:20 +0300

    вычислить факториал

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,9 +1,64 @@
-function sum(m, n) {
-    return m + n;
+'use strict'
+
+function factorial(n) {
+    let sum = 1;
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        for (let i=2; i <= n; i++) {
+            sum=sum * i; 
+        }
+        return sum;
+    };
+};
+
+alert(factorial(1));
+alert(factorial(0));
+alert(factorial(6));
+
+function sayHello() {
+    let userName = prompt('Введите ваше имя');
+  
+    if ( userName=='' || userName == null || userName.length < 4) {
+        alert('Некорректное имя');
+    } else {
+      alert(`Welcome back, ${userName}!`)  
+    }
+  }
+  
+  sayHello();
+
+
+function ucFirst(str) {
+    let string = str.toUpperCase();
+    alert(string);
 }
 
-let result = sum(1, 1);
-let result1 = sum(2, 5);
+ucFirst('вася');
+ucFirst('');
 
-alert (result);
-alert (result1);
+function checkSpam(strr) {
+    let name = strr.toLowerCase();
+    if (name == '1xbet now' || name == 'free xxxxx') {
+        return true;
+    } else {
+        return false;
+    };
+};
+
+alert(checkSpam('1XBeT now'));
+alert(checkSpam('free xxxxx'));
+alert(checkSpam('innocent rabbit')); 
+
+
+function truncate(str, maxlength) {
+    if (str.length > maxlength) {
+        str = str.substring(0, maxlength)+ String.fromCharCode(8230);
+        return str;
+    } else {
+        return str;
+    }
+}
+
+alert(truncate('Вот, что мне хотелось бы сказать на эту тему:', 20));
+alert(truncate('Вот, что мне', 20));
--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,17 @@
+'use strict'
+
 function factorial(n) {
-  // ваш код...
-}
+    let sum = 1;
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        for (let i=2; i <= n; i++) {
+            sum=sum * i; 
+        }
+        return sum;
+    };
+};
+
+alert(factorial(1));
+alert(factorial(0));
+alert(factorial(6));
