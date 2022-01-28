8a7b458bcb86dc09d6a50d2830889e4a8c7eeabe
EdWright <snl290995@gmail.com>
2022-01-28 21:17:07 +0300

    Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,13 @@
 function checkSpam(str) {
-  // ваш код...
-}
+    let name = str.toLowerCase();
+    if (name == '1xbet now' || name == 'free xxxxx') {
+        return true;
+    } else {
+        return false;
+    };
+};
+
+alert(checkSpam('1XBeT now'));
+alert(checkSpam('free xxxxx'));
+alert(checkSpam('innocent rabbit')); 
+
