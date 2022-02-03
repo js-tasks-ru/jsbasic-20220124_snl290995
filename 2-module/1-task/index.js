e8f11b71007274b0a0731c0579287f4750d2225c
EdWright <snl290995@gmail.com>
2022-02-03 18:20:34 +0300

    Cумма свойств в объекте

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,30 @@
-function sumSalary(salaries) {
-  // ваш код...
+'use strict'
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+function sumSalary(salaries) {
+    let sumOfSalaries = 0;
+
+    for (let key in salaries) {
+      let isSalary = typeof salaries[key] === 'number';
+
+      if (isSalary) {
+        sumOfSalaries += salaries[key];
+      }
+    }
+    return sumOfSalaries;
+}
+
+
+let result = sumSalary(salaries);
+alert(result);
+
+
+
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,18 @@
+'use strict'
+
 function isEmpty(obj) {
-  // ваш код...
+    for (let prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        return false;
+    }
 }
+return true;
+}
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); 
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) ); 
