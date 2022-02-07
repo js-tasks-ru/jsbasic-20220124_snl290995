73a9ebd8eacff540acdf804a6094d4a3dfff2cb6
EdWright <snl290995@gmail.com>
2022-02-07 12:33:40 +0300

    Первая задача

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -2,14 +2,15 @@
 let inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
 
 function getMinMax(str) {
-   return str
-   .split(' ')
-   .map ( item => {
-   let max = Math.max.apply(null, item)
-   let min = Math.min.apply(null, item)
-   return {max, min}
-   })
-   .join('\n')
+      let stringWithoutSpaces = str.split(' ').join();
+      let itemsDividedByComma = stringWithoutSpaces.split(',');
+      let numbersOnly = itemsDividedByComma
+        .filter((item) => item !== '' && isFinite(item));
+    
+      let max = Math.max(...numbersOnly);
+      let min = Math.min(...numbersOnly);
+    
+      return {min, max};
 }
 
 console.log(getMinMax(inputData));
