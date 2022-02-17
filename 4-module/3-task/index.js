7d7c8e98527c3b1a9cbe49283729a6ebd835ed1b
EdWright <snl290995@gmail.com>
2022-02-17 20:41:39 +0300

    решил задачи

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,32 +1,33 @@
 function highlight(table) {
   
-let tbody = table.getElementsByTagName('tbody')[0];
-  let trs = tbody.getElementsByTagName('tr');
-
-    Array.from(trs).forEach(
-        (row) => {
-            const statusCell = row.getElementsByTagName('td')[3];
-            if(!statusCell.dataset.available) {
-                row.hidden = true;
-            }
-            else if(statusCell.dataset.available === "true") {
-                row.classList.add('available');
-            }
-            
-            const genderCell = row.getElementsByTagName('td')[2];
-            if(genderCell.textContent === 'm') {
-                row.classList.add('male');
-            }
-            else if(genderCell.textContent === 'f') {
-                row.classList.add('female');
-            }
-            
-            const ageCell = row.getElementsByTagName('td')[1];
-            if(Number(ageCell.textContent) < 18) {
-                row.style.textDecoration = 'line-through';
-            }
-        }
-    );
-
-   return trs;
-}
+  let tbody = table.getElementsByTagName('tbody')[0];
+    let trs = tbody.getElementsByTagName('tr');
+  
+      Array.from(trs).forEach(
+          (row) => {
+              const statusCell = row.getElementsByTagName('td')[3];
+              if(!statusCell.dataset.available) {
+                  row.hidden = true;
+              }
+              else if(statusCell.dataset.available === "true") {
+                  row.classList.add('available');
+              }
+              
+              const genderCell = row.getElementsByTagName('td')[2];
+              if(genderCell.textContent === 'm') {
+                  row.classList.add('male');
+              }
+              else if(genderCell.textContent === 'f') {
+                  row.classList.add('female');
+              }
+              
+              const ageCell = row.getElementsByTagName('td')[1];
+              if(Number(ageCell.textContent) < 18) {
+                  row.style.textDecoration = 'line-through';
+              }
+          }
+      );
+  
+     return trs;
+  }
+  
